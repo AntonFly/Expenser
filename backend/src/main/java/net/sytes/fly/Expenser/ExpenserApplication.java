3696e4e42ac11e3319e179a335c101843c206b49
AntonFly <anton99910@gmail.com>
2024-02-17 20:51:59 +0300

    Initial commit

---
+++ backend/src/main/java/net/sytes/fly/Expenser/ExpenserApplication.java
@@ -0,0 +1,16 @@
+package net.sytes.fly.Expenser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {
+		DataSourceAutoConfiguration.class})
+
+public class ExpenserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenserApplication.class, args);
+	}
+
+}
